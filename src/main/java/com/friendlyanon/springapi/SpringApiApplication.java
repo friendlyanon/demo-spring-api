63f0a1dcda8d68259c978c34264d58da63094cf5
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-07-03 13:50:06 +0200

    Make sure the application runs with assertions enabled

--- src/main/java/com/friendlyanon/springapi/SpringApiApplication.java
+++ src/main/java/com/friendlyanon/springapi/SpringApiApplication.java
@@ -8,6 +8,16 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 public class SpringApiApplication {
     public static void main(String[] args) {
+        ensureAssert();
         SpringApplication.run(SpringApiApplication.class, args);
+    }
+
+    private static void ensureAssert() {
+        try {
+            assert false;
+            throw new AssertionError("assert keyword must be enabled");
+        } catch (AssertionError expected) {
+            //
+        }
     }
 }
