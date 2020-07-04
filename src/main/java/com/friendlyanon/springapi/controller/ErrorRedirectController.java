b9cf6352e328828d3df94ccf4030caef320e1285
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-07-04 20:14:43 +0200

    Remove default value

--- src/main/java/com/friendlyanon/springapi/controller/ErrorRedirectController.java
+++ src/main/java/com/friendlyanon/springapi/controller/ErrorRedirectController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrorRedirectController implements ErrorController {
-    @RequestMapping("${server.error.path:error}")
+    @RequestMapping("${server.error.path}")
     public String error() {
         return "redirect:/";
     }
