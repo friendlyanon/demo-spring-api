ea4857fe36b7c7c703f253960855815646842841
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-07-14 01:39:10 +0200

    Give the secret property a default value

--- src/main/java/com/friendlyanon/springapi/property/SecretProperties.java
+++ src/main/java/com/friendlyanon/springapi/property/SecretProperties.java
@@ -1,14 +1,14 @@
 package com.friendlyanon.springapi.property;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@AllArgsConstructor
 @Configuration
 @ConfigurationProperties("secrets")
+@NoArgsConstructor
 @Getter
 @Setter
 public class SecretProperties {
@@ -16,5 +16,5 @@ public class SecretProperties {
      * Clients must send this key with the x-hash-auth-key header if they wish
      * to persist new hashes into the database.
      */
-    private String authKey;
+    private String authKey = "";
 }
